9ee4b776f5214ca6a988307cc4798a0124ad4a45
ThairConstante <thairconstante123@gmail.com>
2026-09-03 09:21:59 -0500

    Update Api

--- app/routes/sensor_routes.py
+++ app/routes/sensor_routes.py
@@ -4,64 +4,69 @@ from sqlalchemy.orm import Session
 from app.core.auth_token import decode_token
 from app.core.config import get_db
 
-import app.crud.usuario_crud as crud
-import app.schemas.usuario_schemas as schemas
+import app.crud.sensor_crud as crud
+import app.schemas.sensor_schema as schemas
 
 
 app = APIRouter()
 
 
 @app.get("/list", dependencies=[Depends(decode_token)])
-def list_users(db: Session = Depends(get_db)):
-    users = crud.get_usuarios(db=db)
-    return users
+def list_sensores(db: Session = Depends(get_db)):
+    return crud.get_sensores(db=db)
 
 
-@app.get("/userId/{usuario_id}", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioBase)
-def id_user(usuario_id: int, db: Session = Depends(get_db)):
-    user = crud.get_usuario(db=db, usuario_id=usuario_id)
-
-    if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="Usuario no encontrado"
-        )
-
-    return user
-
-
-@app.post("/create", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioCreate)
-def user_create(
-    user: schemas.UsuarioCreate,
+@app.get(
+    "/{sensor_id}",
+    dependencies=[Depends(decode_token)],
+    response_model=schemas.SensorResponse
+)
+def get_sensor(
+    sensor_id: int,
     db: Session = Depends(get_db)
 ):
-    return crud.crear_usuario(db=db, user=user)
 
-
-@app.put("/update/{usuario_id}", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioBase)
-def user_update( usuario_id: int, user: schemas.UsuarioUpdate, db: Session = Depends(get_db)):
-    users = crud.actualizar_usuario(
+    sensor = crud.get_sensor(
         db=db,
-        usuario_id=usuario_id,
-        user=user
+        sensor_id=sensor_id
     )
 
-    if users is None:
+    if sensor is None:
         raise HTTPException(
             status_code=404,
-            detail="Usuario no encontrado"
+            detail="Sensor no encontrado"
         )
 
-    return users
+    return sensor
 
 
-@app.get("/types", dependencies=[Depends(decode_token)])
-def list_user_types(db: Session = Depends(get_db)):
-    tipos = crud.get_tipos(db)
-    return tipos
+@app.post(
+    "/create",
+    dependencies=[Depends(decode_token)],
+    response_model=schemas.SensorResponse
+)
+def create_sensor(
+    sensor: schemas.SensorCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.crear_sensor(
+        db=db,
+        sensor=sensor
+    )
 
 
-@app.get("/statuses", dependencies=[Depends(decode_token)])
-def list_user_statuses(db: Session = Depends(get_db)):
-    estados = crud.get_estados(db)
-    return estados
+@app.put(
+    "/update/{sensor_id}",
+    dependencies=[Depends(decode_token)],
+    response_model=schemas.SensorResponse
+)
+def update_sensor(
+    sensor_id: int,
+    sensor: schemas.SensorUpdate,
+    db: Session = Depends(get_db)
+):
+    return crud.actualizar_sensor(
+        db=db,
+        sensor_id=sensor_id,
+        sensor=sensor
+    )
--- app/routes/usuario_routes.py
+++ app/routes/usuario_routes.py
@@ -1,47 +1,43 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-
 from sqlalchemy.orm import Session
-from app.core.auth_token import decode_token, Annotated
-from app.core.config import SessionLocal, engine, get_db
 
-from app.models.user_model import Base
-
-import app.crud.user_crud as crud
-import app.schemas.user_schemas as schemas
+from app.core.auth_token import decode_token
+from app.core.config import get_db
+import app.crud.usuario_crud as crud
+import app.schemas.usuario_schemas as schemas
 
 app = APIRouter()
 
 @app.get("/list", dependencies=[Depends(decode_token)])
 def list_users(db: Session = Depends(get_db)):
-    users = crud.get_usuarios(db=db)
-    return users
+    return crud.get_usuarios(db=db)
 
-@app.get("/userId/{user_id}", dependencies=[Depends(decode_token)], response_model=schemas.UserBase)
-def id_user(user_id: int, db: Session = Depends(get_db)):
-    users = crud.get_usuario(db=db, user_id=user_id)
-    if users is None:
+@app.get("/userId/{usuario_id}", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioBase)
+def id_user(usuario_id: int, db: Session = Depends(get_db)):
+    usuario = crud.get_usuario(db=db, usuario_id=usuario_id)
+
+    if usuario is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return users
 
-@app.post("/create", dependencies=[Depends(decode_token)], response_model=schemas.UserCreate)
-def user_create(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return usuario
+
+@app.post("/create", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioCreate)
+def user_create(user: schemas.UsuarioCreate, db: Session = Depends(get_db)):
     return crud.crear_usuario(db=db, user=user)
 
-@app.put("/update/{user_id}", dependencies=[Depends(decode_token)], response_model=schemas.UserBase)
-def user_update(user_id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
-    users = crud.actualizar_usuario(db=db, user_id=user_id, user=user)
-    if users is None:
+@app.put("/update/{usuario_id}", dependencies=[Depends(decode_token)], response_model=schemas.UsuarioBase)
+def user_update(usuario_id: int, user: schemas.UsuarioUpdate, db: Session = Depends(get_db)):
+    usuario = crud.actualizar_usuario(db=db, usuario_id=usuario_id, user=user)
+
+    if usuario is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
-    return users
+
+    return usuario
 
 @app.get("/types", dependencies=[Depends(decode_token)])
 def list_user_types(db: Session = Depends(get_db)):
-    tipos = crud.get_tipos(db)
-    return tipos
-
+    return crud.get_tipos(db)
 
 @app.get("/statuses", dependencies=[Depends(decode_token)])
 def list_user_statuses(db: Session = Depends(get_db)):
-    estados = crud.get_estados(db)
-    return estados
+    return crud.get_estados(db)
