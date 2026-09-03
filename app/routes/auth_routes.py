5157529a55c302d12ca2167b07e3734f0d4ac3e7
ThairConstante <thairconstante123@gmail.com>
2026-09-03 09:33:01 -0500

    Update login

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.core.auth_token import decode_token, encode_token, Annotated
-from app.core.config import get_db
 
+from app.core.auth_token import decode_token, encode_token
+from app.core.config import get_db
 import app.crud.auth_validacion as crud
 import app.schemas.auth_schemas as schemas
 
@@ -10,17 +10,20 @@ app = APIRouter()
 
 @app.post("/login")
 def login(user_data: schemas.UserLogin, db: Session = Depends(get_db)):
+
     user = crud.login_user(db, user_data.username, user_data.password)
+
     if not user:
         raise HTTPException(status_code=401, detail="Credenciales incorrectas")
-    token = encode_token({"username": user.User_Name})
-    #return {"access_token": token, "token_type": "bearer"}
+
+    token = encode_token({"username": user.Usuario_Nombre})
+
     return {
         "access_token": token,
         "token_type": "bearer",
         "user": {
-            "User_Id": user.User_Id,
-            "User_Names": user.User_Names,
-            "Usertype_Id": user.Usertype_Id
+            "Usuario_Id": user.Usuario_Id,
+            "Usuario_Nombres": user.Usuario_Nombres,
+            "TipoUsuario_Id": user.TipoUsuario_Id
         }
     }
